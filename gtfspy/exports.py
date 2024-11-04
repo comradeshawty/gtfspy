bed8849618db6c794dbeed39480233a8ce928c6e
comradeshawty <ism3@rice.edu>
2024-11-04 00:15:30 -0600

    Update exports.py

--- gtfspy/exports.py
+++ gtfspy/exports.py
@@ -22,7 +22,7 @@ def write_walk_transfer_edges(gtfs, output_file_name):
     output_file_name: str
     """
     transfers = gtfs.get_table("stop_distances")
-    transfers.drop([u"min_transfer_time", u"timed_transfer"], 1, inplace=True)
+    transfers.drop([u"min_transfer_time", u"timed_transfer"], axis=1, inplace=True)
     with util.create_file(output_file_name, tmpdir=True, keepext=True) as tmpfile:
         transfers.to_csv(tmpfile, encoding='utf-8', index=False)
 
